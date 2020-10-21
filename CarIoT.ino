553c7614197f52ba1dbb1c991acf5f7613e64666
vietvd13 <vuducviet0131@gmail.com>
2020-10-21 08:53:01 +0700

    Update code control voice

--- CarIoT.ino
+++ CarIoT.ino
@@ -7,10 +7,11 @@
 
 SoftwareSerial bluetooth(D6, D5);
 //Define Firebase
-#define FIREBASE_HOST "https://project-demo-e0f9a.firebaseio.com/"
+#define FIREBASE_HOST "project-demo-e0f9a.firebaseio.com"
 #define FIREBASE_AUTH "SYE0NrsPDGNsCYKeLAvpCsxhiaQ8Vic6kgkAOYkD"
 #define WIFI_SSID "Minh Quan"
 #define WIFI_PASSWORD "13102020"
+
 //Define motor
 #define IN1 D1
 #define IN2 D2
@@ -26,6 +27,7 @@ char key;
 void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
+  //pinMode MQ
   pinMode(MQ, INPUT);
   //Connect to Wifi
   pinMode(D0, OUTPUT);
@@ -46,15 +48,14 @@ void setup(){
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
 }
 
 void loop(){
-  //Send data MQ to Firebase
-  dataMQ = analogRead(MQ);
-  myString = String(dataMQ);
-  Firebase.setString("MQ Value", myString);
-  delay(500);
-  
   //Control car
   if(bluetooth.available()){
     key = bluetooth.read();
@@ -69,32 +70,77 @@ void loop(){
     goLeft();
   } else if(key == 'B'){
     goRight();
+  } else if(key == 'L'){
+    goTopgg();
+  } else if(key == 'X'){
+    goBackgg();
+  } else if(key == 'T'){
+    goLeftgg();
+  } else if(key == 'P'){
+    goRightgg();
   } else {
     goStop();
   }
+  
+  //Send data MQ to Firebase
+  sendDataMQ();
 }
-
+// Run when command is Top
 void goTop(){
   digitalWrite(IN1, HIGH);
   digitalWrite(IN3, HIGH);
 }
-
+// Run when command is Back
 void goBack(){
   digitalWrite(IN2, HIGH);
   digitalWrite(IN4, HIGH);
 }
-
+// Run when command is Left
 void goLeft(){
   digitalWrite(IN3, HIGH);
 }
-
+// Run when command is Right
 void goRight(){
   digitalWrite(IN1, HIGH);
 }
-
+// Run when key is not equals with command
 void goStop(){
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, LOW);
+  key = 's';
+}
+// Run when Google A -> call Top
+void goTopgg() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN3, HIGH);
+  delay(1000);
+  goStop();
+}
+// Run when Google A -> call Back
+void goBackgg() {
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN4, HIGH);
+  delay(1000);
+  goStop();
+}
+// Run when Google A -> call Left
+void goLeftgg() {
+  digitalWrite(IN3, HIGH);
+  delay(500);
+  goStop();
+}
+// Run when Google A -> call Right
+void goRightgg() {
+  digitalWrite(IN1, HIGH);
+  delay(500);
+  goStop();
+}
+// Send data read in MQ to Firebase
+void sendDataMQ(){
+  dataMQ = analogRead(MQ);
+  myString = String(dataMQ);
+  Serial.print(myString);
+  Firebase.setString("MQ Value", myString);
 }
